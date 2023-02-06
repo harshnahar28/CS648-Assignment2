3fea92b514d7dacde08fd1c49910a1b4fc814a81
harshnahar28 <103315043+harshnahar28@users.noreply.github.com>
2023-02-05 22:08:21 -0800

    changes reflected

---
+++ Part1/js/functions-practice.js
@@ -0,0 +1,48 @@
+/*eslint-env browser*/
+
+//STEP 1
+var number;
+
+function halfNumber() {
+    "use strict";
+    window.document.write("Half of " + number + " is " + number / 2 + ".<br>");
+}
+
+number = parseFloat(window.prompt("STEP 1. Half of a number. \nEnter a number:"));
+halfNumber();
+
+//STEP 2
+var number;
+function squareNumber() {
+    "use strict";
+    window.document.write("The result of squaring the number " + number + " is " + number * number + ".<br>");
+}
+
+number = parseFloat(window.prompt("STEP 2. Squaring a number. \nEnter a number:"));
+squareNumber();
+
+//STEP 3
+var firstNumber, secondNumber;
+
+function percentOf() {
+    "use strict";
+    window.document.write(firstNumber + " is " + firstNumber / secondNumber * 100 + "% of " + secondNumber + ".<br>");
+}
+
+firstNumber = parseFloat(window.prompt("STEP 3. Percent the first number represents of the second number. \nEnter a first number:"));
+secondNumber = parseFloat(window.prompt("Enter a second number:"));
+percentOf();
+
+//STEP 4
+var firstModNumber;
+var secondModNumber;
+
+function findModulus() {
+    "use strict";
+    var mod = firstModNumber % secondModNumber;
+    window.document.write(mod + " is the modulus of " + firstModNumber + " and " + secondModNumber + ".<br>");
+}
+
+firstModNumber = parseFloat(window.prompt("STEP 4. The modulus of the two numbers. \nEnter a first number:"));
+secondModNumber = parseFloat(window.prompt("Enter a second number:"));
+findModulus();
